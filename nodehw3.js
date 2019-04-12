34556ee8cb26c1763610edde5cd66ab713352e72
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-04-12 01:07:23 -0500

    complete v1

---
+++ nodehw3.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+const fs = require('fs');
+var contents = fs.readFileSync('departments.json', 'utf8');
+app.get('/', function (req, res) {
+    // res.send(contents)
+    res.sendFile('HW3 Heroku.html' , { root : __dirname});
+});
+
+app.get('/json', function (req, res) {
+    return res.json(contents)
+});
+app.listen(8080, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on port on 8080");
+    }
+});
